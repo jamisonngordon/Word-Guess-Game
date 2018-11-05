d3c4cbb3b6cce647a4062f026f0b6562f0299a5c
Jamison Gordon <jgordon@uhin.org>
2018-11-05 16:57:19 -0700

    Added word retriever, checked if the input key is only a letter or number and put a special key checker

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,65 @@
+
+var word;
+var guessed = "";
+
 document.addEventListener("DOMContentLoaded", function() {
     console.log(words);
+    word = words[Math.floor(Math.random() * words.length)];
+    console.log(word);
+    setWord(word);
   });
+
+  function setWord(word) {
+    var wordWithDashes = "";
+    for(x = 0; x < word.length; x++)
+    {
+        if(word.charAt(x) != " ")
+        {
+            wordWithDashes  += "-";
+        }
+        else 
+        {
+            wordWithDashes += " ";
+        }
+    }
+    document.getElementById("word").innerHTML = "Word: " + wordWithDashes;
+  }
+
+  document.onkeyup = function(event) {
+    if(notControlKey(event.key))
+    {
+        var input = isLetterOrNumber(event.key);
+        if(input) 
+        {
+            guessed += input;
+            document.getElementById("guessed").innerHTML = "Letters already guessed: " + guessed;
+        }
+    }
+  };
+
+  //figures out if a character is a letter or number and returns the input if it is, false otherwise
+  function isLetterOrNumber (char) {
+    if(char.match(/^[A-Za-z0-9]/) !== null)
+    {
+        return char;
+    }
+    else 
+    {
+        return false;
+    }
+  }
+
+  function notControlKey(string)
+  {
+      var notWanted = ["Backspace", "Alt", "Shift", "Control", "Meta", "Enter", "Tab", "CapsLock", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+      console.log("String is: " + string);
+      for(x = 0; x < notWanted.length; x++)
+      {
+        console.log("Term is: " + notWanted[x]);
+        if(string === notWanted[x]) 
+        {
+            return false;
+        }
+      }
+      return true;
+  }
